21a8e00325ff70ea0c0edcab9f0fe9b2c262bf1b
Adel Hassan <sean.dashals@gmail.com>
2019-01-27 10:21:35 -0600

    now saves pictures

--- server.py
+++ server.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask import jsonify
 from flask import request
 from flask_cors import CORS
+import base64
 from SP import predict_prices, date_to_int
 from Twitter import search_twitter
 
@@ -35,12 +36,17 @@ def predict(symbol, date):
     return jsonify({'price': predicted_price, 'xs': xs.tolist(), 'ys': ys, 'tweet': tweet, 'sentiment': sentiment})
 
 
-@app.route('/register', methods=['POST'])
-def register():
+@app.route('/register/<string:username>', methods=['POST'])
+def register(username):
     if request.method == 'POST':
-        f = request.files['face_image']
-        print('face_images/' + request.args.get('username') + '.jpg')
-        f.save('face_images/'+request.args.get('username')+'.jpg')
+        # f = request.files['face_image']
+        # print('face_images/' + request.args.get('username') + '.jpg')
+        # f.save('face_images/'+request.args.get('username')+'.jpg')
+        content = request.get_json()
+        img_data = content['img'].split('base64,')[1]
+        with open('face_images/'+username+'.png', 'wb') as fh:
+            fh.write(base64.b64decode(img_data))
+        return 'OK'
 
 
 if __name__ == '__main__':
